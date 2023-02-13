aedd72377915a06fbd1f2711eda5eed29595e9b7
Dheeraj <anubhavgupta1012@gmail.com>
2023-02-13 11:30:48 +0530

    Formatting Changes

--- src/main/java/java8/threading/ShutDownHookUp/HookUpTest.java
+++ src/main/java/java8/threading/ShutDownHookUp/HookUpTest.java
@@ -1,18 +1,18 @@
 package java8.threading.ShutDownHookUp;
 
-class ShutdownHook extends Thread
-{ public void run()
-{ System.out.println("HookUp thread Started");
-System.out.println("Cleaning tasks have been Started");
-}}
+class ShutdownHook extends Thread {
+    public void run() {
+        System.out.println("HookUp thread Started");
+        System.out.println("Cleaning tasks have been Started");
+    }
+}
 
-class HookUpTest
-{ 
- public static void main(String S[])
-{ System.out.println("maain method started");
-ShutdownHook hk=new ShutdownHook();
-Runtime r=Runtime.getRuntime();
-r.addShutdownHook(hk);
-System.out.println("main is about to finish");
-
-}}
+class HookUpTest {
+    public static void main(String S[]) {
+        System.out.println("maain method started");
+        ShutdownHook hk = new ShutdownHook();
+        Runtime r = Runtime.getRuntime();
+        r.addShutdownHook(hk);
+        System.out.println("main is about to finish");
+    }
+}
